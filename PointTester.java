738483e01770aac61ffaa9722c06eaaebf371ac0
Tariq Naseem <tariqnaseem333@gmail.com>
2023-11-12 13:28:34 +0530

    OOP Constructs

---
+++ PointTester.java
@@ -0,0 +1,18 @@
+package sunday_11_nov;
+
+public class PointTester {
+
+	public static void main(String[] args) {
+		
+		Point p1 = new Point();
+		p1.setX(2);
+		p1.setY(3);
+		System.out.println("The distance of the Point p1 from origin is: " + p1.distance());
+
+		Point p2 = new Point( 5, 6 );
+		System.out.println("The distance of the Point p1 from Point p2 is: " + p2.distance(p1));
+
+
+	}
+
+}
