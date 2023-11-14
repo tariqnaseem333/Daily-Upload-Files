540a994a82d9bf9ff5a92516acf3250d664ee876
Tariq Naseem <tariqnaseem333@gmail.com>
2023-11-14 11:52:39 +0530

    package and access modifiers

---
+++ StudentDetails.java
@@ -0,0 +1,21 @@
+package com.infy.package2;
+
+import com.infy.package1.Student;
+
+public class StudentDetails {
+	
+//	Methods
+	public void calculatePercentage( Student student ) {
+		double percentage = ( student.getTotalMarksObtained() / student.getTotalMarks() ) * 100.0;
+		System.out.println("Percentage: " + Math.round(percentage*100) / 100.0);
+	}
+	
+	public void displayStudentDetails( Student s ) {
+		System.out.println("Student Details:");
+		System.out.println("	Student ID: " + s.getStudentId());
+		System.out.println("	Student Name: " + s.getStudentName());
+		System.out.println("	Student Marks: " + s.getTotalMarksObtained());
+		System.out.println("	Total Marks: " + s.getTotalMarks());
+	}
+
+}
