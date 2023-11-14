5a9f2b13af03ebbbb45276e8a8bdb516e7ae50f8
Tariq Naseem <tariqnaseem333@gmail.com>
2023-11-14 14:21:33 +0530

    string class

---
+++ Customer.java
@@ -0,0 +1,50 @@
+package tuesday_14_nov;
+
+public class Customer{
+	
+//	Instance  Variables
+	private String customerName;
+	private int applicableDiscount;
+	
+//	Methods
+	public void findDiscountAndName(String inputString) {
+		
+		String[] customer = inputString.trim().split(":");
+		this.setCustomerName(customer[1]);
+		if( customer[0].equals("Regular") ) {
+			this.setApplicableDiscount(10);
+		} else if( customer[0].equals("Privileged") ) {
+			this.setApplicableDiscount(20);
+		} else {
+			System.out.println("Invalid Customer Name!!");
+		}
+		
+//		String str = inputString.trim();
+//		int n = str.indexOf(":");
+//		this.setCustomerName(str.substring(n+1, str.length()));
+//		String customerType = str.substring(0, n);
+//		if( customerType.equals("Regular") ) {
+//			this.setApplicableDiscount(10);
+//		} else if( customerType.equals("Privileged") ) {
+//			this.setApplicableDiscount(20);
+//		} else {
+//			this.setApplicableDiscount(0);
+//		}
+		
+	}
+	
+//	Getters and Setters
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public int getApplicableDiscount() {
+		return applicableDiscount;
+	}
+	public void setApplicableDiscount(int applicableDiscount) {
+		this.applicableDiscount = applicableDiscount;
+	}
+	
+}
